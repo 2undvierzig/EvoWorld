da1390e87b52c288eca6dba8390eea18837a6b7c
2undvierzig <juschkin@gmx.de>
2023-08-21 01:28:44 +0200

    another fastapi example added

--- fastapi_example/main.py
+++ fastapi_example/main.py
@@ -2,18 +2,24 @@ from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 import random
 import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-#print("Hello start")
+logger.info("Hello start")
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    print("WebSocket connection established.")
+    logger.info("WebSocket connection established.")
 
     particle_data = {"x": 150, "y": 150}
-
+    
     print("Starting loop for sending updates...")
 
     while True:
@@ -39,6 +45,13 @@ async def websocket_endpoint(websocket: WebSocket):
         particle_data["y"] = max(0, min(300, particle_data["y"]))
 
         await websocket.send_json(particle_data)
-        print(f"Sent updated particle data to client: {particle_data}")
+        logger.info(f"Sent updated particle data to client: {particle_data}")
 
         await asyncio.sleep(1)
+        
+
+@app.get("/test")
+def test_endpoint():
+    print("test endpoint printed")
+    logger.info("Test endpoint hit!")
+    return {"message": "Test successful"}
---
+++ fastapi_example2/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.staticfiles import StaticFiles
+import json
+import random
+import asyncio
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+particles = []
+for i in range(1,500):
+    p = {"id": i, "x": random.randint(100, 700), "y": random.randint(100, 700)}
+    particles.append(p)
+
+"""particles = [
+    {"id": 1, "x": random.randint(100, 200), "y": random.randint(100, 200)},
+    {"id": 2, "x": random.randint(100, 200), "y": random.randint(100, 200)}
+]"""
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("WebSocket connection established.")
+    
+    # Send initial particle data
+    await websocket.send_json(particles)
+    print(f"Sent initial particle data to client: {particles}")
+
+    # Update particles
+    while True:
+        for particle in particles:
+            particle["x"] += random.randint(-100, 100)
+            particle["y"] += random.randint(-100, 100)
+        
+        await websocket.send_json(particles)
+        print(f"Sent updated particle data to client: {particles}")
+        await asyncio.sleep(1)
