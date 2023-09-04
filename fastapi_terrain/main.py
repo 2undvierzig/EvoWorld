368a1bf35501263fa0fc2073ded4897473d59f15
2undvierzig <juschkin@gmx.de>
2023-09-04 22:56:26 +0200

    Zooming function added

--- fastapi_terrain/main.py
+++ fastapi_terrain/main.py
@@ -4,6 +4,7 @@ import pika
 import json
 import base64
 import logging
+from fastapi.responses import RedirectResponse
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,28 +12,42 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def get_terrain_from_rabbitmq():
-    # Setup the connection to RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
+    try:
+        # Setup the connection to RabbitMQ
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
 
-    # Get the message from the terrain_init queue
-    method_frame, header_frame, body = channel.basic_get('terrain_init')
-    if method_frame:
-        image_base64 = base64.b64encode(body).decode('utf-8')
-        logging.debug(f"Retrieved terrain data from RabbitMQ: {image_base64[:100]}...")  # Ersten 100 Zeichen anzeigen
-        return {"image": image_base64}
-    else:
-        logging.debug("No message retrieved from RabbitMQ.")
+        # Get the message from the terrain_init queue
+        method_frame, header_frame, body = channel.basic_get('terrain_init')
+        if method_frame:
+            image_base64 = base64.b64encode(body).decode('utf-8')
+            logging.debug(f"Retrieved terrain data from RabbitMQ. Length: {len(image_base64)} chars, Preview: {image_base64[:100]}...")  # Ersten 100 Zeichen anzeigen
+            return {"image": image_base64}
+        else:
+            logging.debug("No message retrieved from RabbitMQ.")
+            return None
+
+    except Exception as e:
+        logging.error(f"An error occurred while fetching terrain from RabbitMQ: {e}")
         return None
 
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/static/index.html")
+    
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    logging.debug("WebSocket connection established.")
-    
-    terrain = get_terrain_from_rabbitmq()
-    if terrain:
-        logging.debug(f"Sending terrain data to client: {terrain}")
-        await websocket.send_json(terrain)
-    else:
-        logging.debug("No terrain data to send.")
+    try:
+        await websocket.accept()
+        logging.debug("WebSocket connection established.")
+        
+        terrain = get_terrain_from_rabbitmq()
+        if terrain:
+            logging.debug(f"Sending terrain data to client. Length: {len(terrain['image'])} chars.")
+            await websocket.send_json(terrain)
+        else:
+            logging.debug("No terrain data to send.")
+
+    except Exception as e:
+        logging.error(f"An error occurred during the WebSocket interaction: {e}")
+
